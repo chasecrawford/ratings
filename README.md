dd88fd8dd61f860ee021cdbb962b8ccf5f1b8956
Chase Crawford <chasecrawford@hatfieldmedia.com>
2022-03-09 16:12:19 -0500

    refactor elo

--- README.md
+++ README.md
@@ -2,49 +2,64 @@
 
 A PHP class which implements the Elo rating system & Rating Percentage index.
 
-# Install
+## Install
 
 ```
 composer require chasecrawford/ratings
 ```
 
-# Usage
+## Using Elo
+
+Calculate a new elo rating for a competitor after a result
 
 ```
 use ChaseCrawford\EloRating;
-use ChaseCrawford\RatingPercentageIndex;
 
 $elo = new EloRating;
+
+$newEloRating = $elo->calculateElo(
+    1000,   // (float) competitor elo rating
+    1000,   // (float) opponent's elo rating
+    71,     // (int) competitor score
+    70,     // (int) opponent score
+    0       // (int) number of matches competitor played previously (optional)
+)
+```
+
+Find the elo ratings for all competitors from a group of results
+
+```
+$results = [...];
+
+foreach($results as $result) {
+  $elo->addResult(
+       $result->competitorOneName,  // (string) unique name for competitor 1
+       $result->competitorTwoName,  // (string) unique name for competitor 2
+       $result->competitorOneScore, // (int) score for competitor 1
+       $result->competitorTwoScore  // (int) score for competitor 2
+  )
+}
+
+print_r($elo->getCompetitors())
+```
+
+## Using RPI
+
+```
+use ChaseCrawford\RatingPercentageIndex;
+
 $rpi = new RatingPercentageIndex;
 
-$games = [
-    [
-        'one' => 'Louisville',
-        'two' => 'Kentucky',
-        'result' => 1 // One Wins (1), Two Wins (0), Draw (0.5)
-    ],
-    ...
-];
+$results = [...];
 
-foreach($games as $game):
-    $elo->addGame(
-        $game['one'], 
-        $game['two'], 
-        $game['result']
-    );
+foreach($results as $result) {
+  $rpi->addGame(
+       $result->competitorOneName,  // (string) unique name for competitor 1
+       $result->competitorTwoName,  // (string) unique name for competitor 2
+       $result->outcome, // 1 for win, 0 for loss, 0.5 for draw
+  )
+}
 
-    $rpi->addGame(
-        $game['one'], 
-        $game['two'], 
-        $game['result']
-    );
-endforeach;
-
-print "<pre>";
-
-print_r($elo);
-print_r($rpi);
-
-print "</pre>";
-exit;
+print_r($rpi)
 ```
+
---
+++ src/EloRating/EloRating.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace ChaseCrawford\EloRating;
+
+class EloRating
+{
+
+    private array $competitors;
+    private int $defaultKFactor;
+    private int $proRating;
+    private int $starterBoundry;
+
+    public function  __construct()
+    {
+        $config = include(dirname(__FILE__) . '/../config/elo.php');
+        $this->defaultKFactor = $config['default_k_factor'];
+        $this->proRating = $config['pro_rating'];
+        $this->starterBoundry = $config['starter_boundry'];
+    }
+
+    public function addResult(
+        string $competitorOneName,
+        string $competitorTwoName,
+        int $competitorOneScore,
+        int $competitorTwoScore
+    ) : void
+    {
+        $competitorOne = $this->getCompetitor($competitorOneName);
+        $competitorTwo = $this->getCompetitor($competitorTwoName);
+        $this->updateCompetitor(
+            $competitorOne,
+            $competitorOneScore,
+            $competitorTwoScore,
+            $competitorOne->getElo(),
+            $competitorTwo->getElo(),
+        );
+        $this->updateCompetitor(
+            $competitorTwo,
+            $competitorTwoScore,
+            $competitorOneScore,
+            $competitorTwo->getElo(),
+            $competitorOne->getElo(),
+        );
+    }
+
+    public function calculateElo(
+        float $competitorOneElo,
+        float $competitorTwoElo,
+        int $competitorOneScore,
+        int $competitorTwoScore,
+        int $competitorOneNumberOfResults = 0
+    ) : float
+    {
+        $expectedResult = $this->getExpectedResult(
+            $competitorOneElo,
+            $competitorTwoElo
+        );
+        $kFactor = $this->getKFactor($competitorOneElo, $competitorOneNumberOfResults);
+        if($competitorOneScore === $competitorTwoScore) {
+            $result = 0.5;
+        } else {
+            $result = $competitorOneScore > $competitorTwoScore
+                ? 1
+                : 0;
+        }
+        return $competitorOneElo + $this->getChange($result, $expectedResult, $kFactor);
+    }
+
+    private function getChange(
+        int $result,
+        float $expectedResult,
+        int $kFactor
+    )
+    {
+        return $kFactor * ($result - $expectedResult);
+    }
+
+    private function getCompetitor(string $competitorName)
+    {
+        return $this->competitors[$competitorName] ?? new Competitor($competitorName);
+    }
+
+    private function getExpectedResult(
+        float $elo,
+        float $opponentElo
+    ) : float
+    {
+        return 1.0 / (1.0 + (10.0 ** (($opponentElo - $elo) / 400.0)));
+    }
+
+    private function getKFactor(float $elo, int $numOfResults)
+    {
+        $kFactor = $this->defaultKFactor;
+        if($elo >= $this->proRating) {
+            $kFactor = 10;
+        }
+        if($numOfResults < $this->starterBoundry) {
+            $kFactor = 25;
+        }
+        return $kFactor;
+    }
+
+    private function updateCompetitor(
+        Competitor $competitor,
+        string $score,
+        string $opponentScore,
+        float $elo,
+        float $opponentElo
+    ) : void
+    {
+        $competitor->setElo(
+            $this->calculateElo(
+                $score,
+                $opponentScore,
+                $elo,
+                $opponentElo,
+                $competitor->numOfResults
+            )
+        );
+        $competitor->numOfResults++;
+        $this->competitors[$competitor->getName()] = $competitor;
+    }
+
+}
